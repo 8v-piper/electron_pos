e8daca222194a49f0998cd3171ddd40859d03470
Rodwyn Hoffman <8v.piper@gmail.com>
2024-01-15 14:30:34 +0200

    users endpoints

---
+++ src/routes/api/db/users/[id].js
@@ -0,0 +1,28 @@
+import { connectToDatabase } from '$lib/db';
+import { ObjectId } from 'mongodb'; // Required for querying by _id
+
+// GET request: Fetch specific user
+export async function get({ params }) {
+    const { db } = await connectToDatabase();
+    const { id } = params;
+    
+    try {
+        const user = await db.collection('users').findOne({ _id: new ObjectId(id) });
+        if (user) {
+            return {
+                status: 200,
+                body: user
+            };
+        } else {
+            return {
+                status: 404,
+                body: { message: 'User not found' }
+            };
+        }
+    } catch (error) {
+        return {
+            status: 500,
+            body: { message: 'Error fetching user' }
+        };
+    }
+}
